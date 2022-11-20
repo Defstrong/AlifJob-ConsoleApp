175c47d5d0bc7a21073ababaca4609cc0987a68b
Deshon <118604174+Defstrong@users.noreply.github.com>
2022-11-20 16:34:39 +0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,9 @@
-﻿
+
 string MyProduct, MyPhoneNumber;
 
 int MySum, MyInstallment;
+
+//Вводим все нужные данные
 
 Console.Write("Enter product type: ");
 MyProduct = Console.ReadLine();
@@ -17,36 +19,51 @@ MyInstallment = Convert.ToInt32(Console.ReadLine());
 
 
 Console.WriteLine();
-Console.WriteLine(GetTotalAmount(MyProduct, MySum, MyPhoneNumber, MyInstallment));
+//Выводим результат
+Console.WriteLine(
+    //Передаём все данные
+    GetTotalAmount(MyProduct, MySum, MyPhoneNumber, MyInstallment));
 
 
 string GetTotalAmount(string TipProduct, int Sum, string PhoneNumbers, int Installment)
 {
+    // InstallmentPercentage это обьший процент рассрочки
     int InstallmentPercentage = 0;
     string Product = "";
+    // Определяем тип продукта
     if (TipProduct == "Phone")
     {
+        
         Product = "Phone";
+        //Передаём данные для получения коэффицента и умножаем на процент товара
         InstallmentPercentage = GetInstallment(9, Installment) * 3;
+
+        //Преобразуем процент в сумму
         InstallmentPercentage = (Sum * InstallmentPercentage) / 100;
     }
     else if (TipProduct == "Komputer")
     {
         Product = "Komputer";
+        //Передаём данные для получения коэфицента и умножаем на процент товара
         InstallmentPercentage = GetInstallment(12, Installment) * 4;
+        //Преобразуем процент в сумму
         InstallmentPercentage = (Sum * InstallmentPercentage) / 100;
     }
     else if (TipProduct == "TV")
     {
         Product = "TV";
+        //Передаём данные для получения коэфицента и умножаем на процент товара
         InstallmentPercentage = GetInstallment(18, Installment) * 5;
+        //Преобразуем процент в сумму
         InstallmentPercentage = (Sum * InstallmentPercentage) / 100;
     }
+    //Возврощаем Eror 404 если товар не найден
     else return "Eror 404. Produkt it's not found";
 
+    //Передаём значения в SendMassege для отправки СМС и возврощаем результат
     return SendMassage(Product, InstallmentPercentage + Sum, Installment);
 
-
+    //Получаем коэффицент процента
     int GetInstallment(int RangeStart, int RangeEnd)
     {
         int Procent = 0;
@@ -59,6 +76,7 @@ string GetTotalAmount(string TipProduct, int Sum, string PhoneNumbers, int Insta
         }
         return Procent;
     }
+    //Отпровляем СМС
     string SendMassage(string TipProduct, int Sum, int Installment)
     {
         return $"Your product: {Product}\nInstallment duration: {Installment} months\nTotal amount: {Sum} smn\n";
